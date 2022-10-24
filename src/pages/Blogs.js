d8ae339cc8c8494afc64cdd080065e226ec03cf2
tsamanth <samantha.tsai.11@gmail.com>
2022-10-23 20:04:20 -0500

    add multipage

---
+++ src/pages/Blogs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Blogs() {
+    return (
+        <h1>this is the blog page</h1>
+    );
+}
+
+export default Blogs;
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ErrorPage() {
+    return (
+        <h1>Error 404</h1>
+    );
+}
+
+export default ErrorPage;
